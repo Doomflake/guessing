131163905f0ca215f024d113ef32e80566e06afd
Doomflake <dewain38@gmail.com>
2018-01-27 23:12:28 -0600

    Added test directories, completed guessing game

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,12 +5,17 @@ echo "Let's play a game!"
 echo "Take a guess of how many directories you have:"; read guess
 
 actual=$(ls -l | grep -c ^d)
-echo $actual
-echo $guess
-if ("$actual" == "$guess") {
-	echo "Good job!  You are correct!"
-else {
-	echo "So sorry, you are wrong!"
-}
 
+while [ $actual -ne $guess ]
+do
+
+if [ "$actual" -gt "$guess" ] 
+then
+	echo "You are too low!  Please try again!"; read guess
+else 
+	echo "You are too high!"
+	echo "Please try again!"; read guess
 fi
+
+done
+echo  "You are correct!   If you survive, please play again!"
